419a2664a13065de1cf5287ea61ad5dfd91df4fc
Obi <john.walton@live.co.uk>
2014-08-08 12:47:08 +0100

    {Edited serverobjects}
    {Edited serverobjects}

--- MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
@@ -1,4 +1,4 @@
 includeFile("event/event_general_grievous.lua")
 includeFile("event/event_super_durni.lua")
 includeFile("event/stresstest_shuttleloc_barker.lua")
-includeFile("event/event_Vader.lua")
+includeFile("event/Vader.lua")
