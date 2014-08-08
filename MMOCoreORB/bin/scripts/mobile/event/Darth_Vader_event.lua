456216d73ba765d3a35db3b96914f003a15914ef
Obi <john.walton@live.co.uk>
2014-08-08 11:03:59 +0100

    {Added new Event mob}
    {Added new Event mob}

---
+++ MMOCoreORB/bin/scripts/mobile/event/Darth_Vader_event.lua
@@ -0,0 +1,38 @@
+Darth_Vader_event = Creature:new {
+	objectName = "@mob/",
+	customName = "Darth_Vader",
+	socialGroup = "imperial",
+	pvpFaction = "imperial",
+	faction = "imperial",
+	level = 300,
+	chanceHit = 300,
+	damageMin = 1645,
+	damageMax = 3000,
+	baseXp = 25000,
+	baseHAM = 385000,
+	baseHAMmax = 550000,
+	armor = 3,
+	resists = {0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 3,
+	pvpBitmask = ATTACKABLE + KILLER,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/darth_vader.iff"},
+	scale = 3,
+	lootGroups = {},
+	weapons = {"dark_jedi_weapons_gen4"},
+	conversationTemplate = "",
+	attacks = merge(lightsabermaster,forcepowermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(Darth_Vader_event,"Darth_Vader_event")
--- MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
@@ -1,3 +1,4 @@
 includeFile("event/event_general_grievous.lua")
 includeFile("event/event_super_durni.lua")
 includeFile("event/stresstest_shuttleloc_barker.lua")
+includeFile("event/Darth_Vader_event.lua")
