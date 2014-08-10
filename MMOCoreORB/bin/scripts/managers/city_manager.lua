2bbcb63471565660d60cb9678c4d45ac067cbb5f
Obi <ObiGr@outlook.com>
2014-08-10 03:33:46 -0700

    {Edited city manager}
    {Edited city manager}

--- MMOCoreORB/bin/scripts/managers/city_manager.lua
+++ MMOCoreORB/bin/scripts/managers/city_manager.lua
@@ -55,17 +55,17 @@ CitySpecializationCooldown = 604800000
 TreasuryWithdrawalCooldown = 86400000
 
 --The number of city update cycles that must pass before mayoral voting process is complete.
-CityVotingCycles = 3
+CityVotingCycles = 1
 
 --The number of city update cyles after which to lock the mayoral voting race registration.
-CityVotingCyclesUntilLocked = 2
+CityVotingCyclesUntilLocked = 1
 
 --The amount of time in minutes before the city performs an update.
 --CityUpdateInterval = 10080
 CityUpdateInterval = 2880
 
 --The amount of time in minutes a new city has to gain enough citizens to remain a city.
-NewCityGracePeriod = 144000
+NewCityGracePeriod = 1440000000000
 
 --The amount of time in minutes an old city has to regain enough citizens to remain a city.
 OldCityGracePeriod = 30240
