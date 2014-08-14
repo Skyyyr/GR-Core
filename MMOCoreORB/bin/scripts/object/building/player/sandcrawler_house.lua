d789572991cacc5eb1e24378a9fca939c8577dec
Freddiec <Socome@users.noreply.github.com>
2014-08-13 22:34:03 -0500

    More Planets for NGE Houses #19

--- MMOCoreORB/bin/scripts/object/building/player/sandcrawler_house.lua
+++ MMOCoreORB/bin/scripts/object/building/player/sandcrawler_house.lua
@@ -44,7 +44,7 @@
 object_building_player_sandcrawler_house = object_building_player_shared_sandcrawler_house:new {
 	lotSize = 2,
 	baseMaintenanceRate = 10,
-	allowedZones = {"naboo", "taanab"},
+	allowedZones = {"naboo", "taanab", "corellia", "tatooine", "rori", "lok", "talus"},
 	publicStructure = 0,
 	skillMods = {
 		{"private_medical_rating", 300},
